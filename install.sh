6f5194c8ae86d4176d21807307dcfca67613c454
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2017-07-13 13:08:07 +0800

    Fix curl path error.

--- install.sh
+++ install.sh
@@ -181,7 +181,7 @@ DOTSPACEVIM
 
 set_dot_spacevim(){
     if [ $TEMPLATE ];then
-        curl -fLo "$TEMPLATE" --create-dirs "$dot_spacevim"
+        curl -fLo "$dot_spacevim" --create-dirs "$TEMPLATE"
     else
         generate_dot_spacevim
     fi
